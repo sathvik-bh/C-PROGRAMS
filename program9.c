c716405036633b208dfebe4b9c16440d214a1e8f
sathvik-bh <sathvikbh2007@gmail.com>
2026-03-23 19:28:38 +0530

    Add program to calculate square and cube of a number

---
+++ program9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    printf("Square = %d\n", n * n);
+    printf("Cube = %d", n * n * n);
+
+    return 0;
+}
