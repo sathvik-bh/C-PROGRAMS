5cac4c0ade4a045290e29de1573045e2662261ee
sathvik-bh <sathvikbh2007@gmail.com>
2026-03-23 19:26:48 +0530

    Add program to convert kilometers to meters and centimeters

---
+++ program6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float km;
+    printf("Enter kilometers: ");
+    scanf("%f", &km);
+
+    printf("Meters = %.2f\n", km * 1000);
+    printf("Centimeters = %.2f\n", km * 100000);
+
+    return 0;
+}
