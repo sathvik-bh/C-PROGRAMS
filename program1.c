1a1317502cf15e43b9c179f03ad8394b680bd08b
sathvik-bh <sathvikbh2007@gmail.com>
2026-03-23 19:22:43 +0530

    Add program1.c to display student information

---
+++ program1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Name: Sathvik\n");
+    printf("USN: YourUSN\n");
+    printf("Branch: AIML\n");
+    printf("College: YourCollege\n");
+    return 0;
+}
